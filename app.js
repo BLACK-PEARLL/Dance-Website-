68c682c3ae4e7dce27562bced432204cc85ad436
Anjali Kumari <iib2019037@iiita.ac.in>
2020-09-25 22:21:03 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const path=require('path');
+const fs=require('fs');
+const app=express();
+const port=8000;
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static',express.static('static'))
+app.use(express.urlencoded())
+
+//PUG SPECIFIC STUFF
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))
+
+//ENDPOINTS
+app.get("/",(req,res)=>{
+   
+    const params={}
+    res.status(200).render('home.pug',params);
+});
+app.get("/contact",(req,res)=>{
+   
+    const params={}
+    res.status(200).render('contact.pug',params);
+});
+
+
+
+//START THE SERVER
+app.listen(port,()=>{
+    console.log(`The appliction started successfully on port ${port}`)
+})
+
